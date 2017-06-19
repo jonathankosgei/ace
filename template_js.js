e236fa260a1b2020e19a7dad2662193c784d90eb
Jonathan Kosgei <kosgei@college.harvard.edu>
2017-06-19 15:40:53 +0300

    added new test webapp files

---
+++ template_js.js
@@ -0,0 +1,4 @@
+console.log("LINKED SUCCESSFULLY: JAVASCRIPT FILE");
+var editor = ace.edit("editor");
+editor.setTheme("./lib/ace/theme/monokai");
+editor.getSession().setMode("./lib/ace/mode/javascript");
